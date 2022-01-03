36cc8563b855dd8404b01fa0f097eabc4c19f888
Vinay kumar <vinayrahar1991@gmail.com>
2022-01-03 22:35:33 +0530

    Add babel to project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 const evelenShieldRecipe = {
     leatherStrips: 2,
     ironIngot: 1,
-    refinedMoonstone: 4
+    refinedMoonstone: 5
+}
+
+const updatedRecipe = {
+    ...evelenShieldRecipe,
+    leather: 1,
+    refinedMoonstone: 3
 }
 
 console.log(evelenShieldRecipe);
+console.log(updatedRecipe);
