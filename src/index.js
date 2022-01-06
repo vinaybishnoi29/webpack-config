e4b19896720591e597642e921413d2c16700e921
Vinay kumar <vinayrahar1991@gmail.com>
2022-01-06 11:56:59 +0530

    Add css, sass, postcss, hotreloading and browserslist

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import './styles/index.scss';
+
 const evelenShieldRecipe = {
     leatherStrips: 2,
     ironIngot: 1,
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); 
+
 let mode = "development";
 
 if(process.env.NODE_ENV === "production") {
@@ -9,16 +11,22 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(s[ac]|c)ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader"]
+      },
+      {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader"
         }
-      }
+      },
     ]
   },
+  plugins: [new MiniCssExtractPlugin()],
   devtool: "source-map",
   devServer: {
-    static: "./dist"
+    static: "./dist",
+    hot: true
   }
 }
