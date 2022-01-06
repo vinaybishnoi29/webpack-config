88de9067dd33ab553e33169193e8efadb33f454f
Vinay kumar <vinayrahar1991@gmail.com>
2022-01-06 23:22:24 +0530

    Fix hot reloading warning for development mode

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,8 @@ const plugins = [
 
 if (process.env.NODE_ENV === "production") {
   mode = "production";
-} else {
+}
+if (process.env.SERVE) {
   plugins.push(new ReactRefreshWebpackPlugin());
 }
 
